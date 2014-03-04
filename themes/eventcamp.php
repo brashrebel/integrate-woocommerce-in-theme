1aae46651bbdb9613e8bd92d201e37979e84dda3
Kyle <kyle@realbigmarketing.com>
2014-03-04 16:00:10 -0500

    Just event camp tweaks
    Removed the sidebar on products and product archives

--- themes/eventcamp.php
+++ themes/eventcamp.php
@@ -1,6 +1,10 @@
 <?php
+// Get body up to snuff and ditch the sidebar on products and product archives
 add_action('woocommerce_before_main_content', 'eventcamp_wrapper_start', 10);
 add_action('woocommerce_after_main_content', 'eventcamp_wrapper_end', 10);
+add_action('wp', create_function("", "if (is_archive(array('product'))) remove_action( 'woocommerce_sidebar', 'woocommerce_get_sidebar', 10);") );
+add_action('wp', create_function("", "if (is_singular(array('product'))) remove_action( 'woocommerce_sidebar', 'woocommerce_get_sidebar', 10);") );
+remove_action( 'woocommerce_sidebar', 'woocommerce_get_sidebar', 10);
 function eventcamp_wrapper_start() {
 echo '<div class="container"><div class="content-wrapper">';
 }
